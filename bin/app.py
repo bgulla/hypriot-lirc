849b6e079cfbdc9f99e12af9701d1f7f83cef766
bgulla <bgulla>
2016-03-20 20:13:01 +0100

    squashing bugs

--- bin/app.py
+++ bin/app.py
@@ -4,9 +4,11 @@ from flask import request
 from flask import abort
 import os
 
-@app.route('/todo/api/v1.0/remote/<string:cmd_code>', methods=['GET'])
+app = Flask(__name__)
+
+@app.route('/bar/api/v1.0/remote/<string:cmd_code>', methods=['GET'])
 def send_remote_code(cmd_code):
-    command = "irsend SEND_ONE led " + cmd_code
+    command = "irsend SEND_ONCE led " + cmd_code
     os.system(command)
     print "[SENT] " + command
 
